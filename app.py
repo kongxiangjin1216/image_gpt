845754c64a5dc0fb71b9da559a3be62ea9d514fe
kongxiangjin1216 <142412229+kongxiangjin1216@users.noreply.github.com>
2023-08-17 11:36:17 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask,render_template,request
+import os
+
+app = Flask(__name__)
+
+import openai
+
+openai.api_key = "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"
+
+@app.route("/",methods=["GET","POST"])
+
+def index():
+    if request.method == "POST":
+        t = request.form.get("txt")
+        response = openai.Image.create(prompt=t,n=1)
+        image_url = response['data'][0]['url']
+        print(image_url)
+        return(render_template("index.html",result=image_url))
+    else:
+        return(render_template("index.html",result="waiting"))
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
